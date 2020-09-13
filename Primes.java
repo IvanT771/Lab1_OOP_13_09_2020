99f5d8e0342fdafefac14c8fec9fff0cdef181fc
князев иван <cnyazev.ivan@yandex.ru>
2020-09-13 22:14:50 +0300

    add function isPrimes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,12 @@
 public class Primes {
- public static void main(String[] args) {
+
+     public static boolean isPrimes(int n){
+        for(int i=2;i<n;i++){
+            if(n%i == 0){return true;}
+        }
+        return false;
+     } 
+     public static void main(String[] args) {
      
  }
 }
