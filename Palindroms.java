2dd712db927cc5d4782e4023dc6637f734e3695b
князев иван <cnyazev.ivan@yandex.ru>
2020-09-14 19:14:34 +0300

    Final Vers

--- Palindroms.java
+++ Palindroms.java
@@ -1,10 +1,39 @@
 public class Palindroms{
 
-    public static void main(String[] args){
+    public static String reverseString(String str){
 
-        for(int i=0;i<args.length;i++){
-            String s = args[i];
+        String s = "";
+
+        for (int i=str.length()-1; i>=0;i--){
+            s+=str.charAt(i);
         }
+        return s;
+    }
+
+    public static boolean isPalindrome(String str){
+
+        if (str.equals(reverseString(str))){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+        String s = "";
+        for(int i=0;i<args.length;i++){
+           
+            s = args[i];
+          
+                if(isPalindrome(s)){
+                    System.out.println(" "+ s +" -YES");
+                }else{
+                    System.out.println(" "+s+" -NO");
+                }
+                s="";
+            }
+            
+        
 
     }
 
