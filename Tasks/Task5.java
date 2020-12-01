f037b009a28193fd7e4910ce3ffde564c8f0674b
Ivan <cnyazev.ivan@yandex.ru>
2020-12-01 23:37:53 +0300

    add task 5.2

--- Tasks/Task5.java
+++ Tasks/Task5.java
@@ -30,6 +30,59 @@ public static int[] encrypt(String str){
 return result;
 }
 
+//Task 5.2
+public static boolean rook(String start, String end){
+    if(start.length() != 2 || end.length() != 2){return false;}
+    if(start.charAt(0) != end.charAt(0)){
+        if (start.charAt(1) != end.charAt(1)){return false;}
+    }
+    return true;
+}
+public static boolean bishop(String start, String end){
+    if(start.length() != 2 || end.length() != 2){return false;}
+    int a = 0;
+    int b = 0;
+
+    a = (int)end.charAt(0) - (int)start.charAt(0) + (int)end.charAt(1);
+    b = (int)start.charAt(1);
+    
+    return a==b;
+}
+public static boolean queen(String start, String end){
+    if(start.length() != 2 || end.length() != 2){return false;}
+    int a = 0;
+
+    a = (int)end.charAt(0) - (int)start.charAt(0) + (int)end.charAt(1) - (int)start.charAt(1);
+    return a<=1;
+}
+public static boolean horse(String start, String end){
+     if(start.length() != 2 || end.length() != 2){return false;}
+     int a = 0;
+     int b = 0;   
+    a = Math.abs((int)end.charAt(0) - (int)start.charAt(0));
+    b = Math.abs((int)end.charAt(1) - (int)start.charAt(1));
+    
+    if ((a == 1 && b == 2) || (a==2 && b==1)) return true;
+    return false;
+}
+public static boolean pawn(String start, String end){
+    if(start.charAt(0) != end.charAt(0)) return false;
+    if(Math.abs((int)start.charAt(1) - (int)end.charAt(1)) != 1) return false;
+    return true;
+}
+public static boolean canMove(String str,String start,String end){
+
+    boolean flag = false;
+    switch (str){
+        case "Rook":{flag = rook(start,end); break;}
+        case "Bishop":{flag = bishop(start,end); break;}
+        case "Queen":{flag = queen(start,end); break;}
+        case "Horse":{flag = horse(start,end); break;}
+        case "Pawn":{flag = pawn(start,end); break;}
+    }
+    return flag;
+}
+
 public static void main(String[] args){
 
 //Task 5.1
@@ -38,6 +91,11 @@ System.out.println(Arrays.toString(encrypt("Hello")));
 System.out.println(decrypt(encrypt("Hello") ));
 //Task 5.2
 System.out.println("Task5.2");
+System.out.println(canMove("Rook", "A8", "H8"));
+System.out.println(canMove("Bishop", "A7", "G1"));
+System.out.println(canMove("Queen", "C4", "D6"));
+//Task 5.3
+System.out.println("Task5.3");
 }
 
 
